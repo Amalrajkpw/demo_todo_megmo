54ab1ba05ee915069b8e3d1ca3955b482d1905cb
Amalraj <amalrajkp24@gmail.com>
2024-06-06 17:21:07 +0530

    utils and config

---
+++ lib/utils/app_sizedbox.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class AppSizedBox {
+  static Widget sizedBox10vertical = SizedBox(
+    height: 10.h,
+  );
+  static Widget sizedBox20vertical = SizedBox(
+    height: 20.h,
+  );
+  static Widget sizedBox30vertical = SizedBox(
+    height: 30.h,
+  );
+  static Widget sizedBox10horizontal = SizedBox(
+    width: 10.w,
+  );
+
+  static Widget sizedBox20horizontal = SizedBox(
+    width: 20.w,
+  );
+  static Widget sizedBox30horizontal = SizedBox(
+    width: 30.w,
+  );
+}
