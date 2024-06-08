80157b9b8cf9f2bc0a970644efeee92ac75e518d
Amalrajkpw <amalrajkp24@gmail.com>
2024-06-08 19:25:39 +0530

    changes in splash screen

--- lib/view/splash_screen/splash_screen.dart
+++ lib/view/splash_screen/splash_screen.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-
-
-
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class SplashScreen extends StatelessWidget {
   @override
@@ -19,29 +17,10 @@ class SplashScreen extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Image.network(
-              'https://cdni.iconscout.com/illustration/premium/thumb/todo-list-5523307-4609476.png', // Make sure to add your image in the assets folder
-              height: 300,
-            ),
-            SizedBox(height: 20),
-            Text(
-              'Welcome to Go Task',
-              style: TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
-                color: Colors.black,
-              ),
-            ),
-            SizedBox(height: 10),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20.0),
-              child: Text(
-                'A workspace to over 10 Million influencers around the global of the world.',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 16,
-                  color: Colors.grey[700],
-                ),
-              ),
+              'https://cdni.iconscout.com/illustration/premium/thumb/todo-list-5523307-4609476.png',
+              // Make sure to add your image in the assets folder
+              height: 300.h,
+              width: double.infinity,
             ),
           ],
         ),
